87d9d2ce0e0f77f69ad500b6cf3d8503f8bdd648
taskylizard <75871323+taskylizard@users.noreply.github.com>
2026-02-21 20:25:46 +0100

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Arkama Rust workspace development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "clippy"
+            "rust-analyzer"
+            "rust-src"
+            "rustfmt"
+          ];
+        };
+
+        runtimeLibs = with pkgs; [
+          alsa-lib
+          libGL
+          libxkbcommon
+          libx11
+          libxcursor
+          libxi
+          libxrandr
+          openssl
+          wayland
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            pkg-config
+          ];
+
+          buildInputs = runtimeLibs;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
